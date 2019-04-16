fa85860401f280852c44e1d1ea8404b5b343fbe1
masterdodo <imdavidarist@gmail.com>
2019-04-16 08:52:49 +0200

    to Github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactHtmlParser from 'react-html-parser';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props)
+  {
+    super(props)
+    this.state =
+    {
+      things: [],
+      text: ''
+    }
+
+  this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event)
+  {
+    this.setState({ text: event.target.value });
+  }
+
+  preracunaj(text)
+  {
+    //alert(this.state.text.length)
+    const abc = this.state.text.split(" ").length
+    const cde = abc
+    //alert(cde)
+    const kolkA = this.state.text.split( new RegExp( "a", "gi" ) ).length-1
+    //alert(kolkA);
+    const kolkE = this.state.text.split( new RegExp( "e", "gi" ) ).length-1
+    //alert(kolkE);
+    const kolkI = this.state.text.split( new RegExp( "i", "gi" ) ).length-1
+    //alert(kolkI);
+    const kolkO = this.state.text.split( new RegExp( "o", "gi" ) ).length-1
+    //alert(kolkO);
+    const kolkU = this.state.text.split( new RegExp( "u", "gi" ) ).length-1
+    //alert(kolkU);
+
+    const text_all = "Text length: " + this.state.text.length + "<br />Word count: " + cde + "<br />How many letters A?" + kolkA + "<br />How many letters E?" + kolkE + "<br />How many letters I?" + kolkI + "<br />How many letters O?" + kolkO + "<br />How many letters U?" + kolkU;
+    alert(text_all)
+    this.setState({
+      things: [...this.state.things, text_all],
+      text: '',
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <textarea rows="30" cols="100" value={this.state.text} onChange={this.handleChange}>
+          </textarea>
+          <button onClick={() => this.preracunaj()}>
+            Preračunaj
+          </button>
+
+          <div id="output-text">
+          {this.state.things.map((thing, index) =>
+            {
+              return (<div class="text-analysis" key={index}>{ ReactHtmlParser(thing) }</div>)
+            }
+            )}
+          </div>
         </header>
       </div>
     );
